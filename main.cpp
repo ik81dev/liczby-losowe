6e9855cf23d33bba7ff34c2b4490e7a7ba5b31e2
kamil <freshmultikk21@gmail.com>
2019-08-04 16:08:08 +0200

    losowanie

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+using namespace std;
+int liczba;
+int main()
+{
+    cout << "witaj w losowaniu za 3 sec nastapi zoninie blokady";
+     cout<<endl;
+    Sleep(3000);
+    srand(time(NULL));
+
+for(int i=1; i<=6; i++)
+   {
+
+   liczba = rand()%49 +1;
+    Sleep(1000);
+
+    cout <<liczba <<"\a"<< endl;
+
+
+
+   }
+
+
+
+
+
+
+
+
+    return 0;
+
+
+}
